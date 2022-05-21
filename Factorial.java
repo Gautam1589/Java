432a449dac9b8d7af18cc3542a38641109e25644
Gautam Yadav <gautamyadav8989@gmail.com>
2022-05-21 14:53:00 +0530

    second

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Factorial {
+    public static int fact(int n){
+        if(n==0) return 1;
+        else return n*fact(n-1);
+    }
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.print(fact(n));
+
+    }
+}
